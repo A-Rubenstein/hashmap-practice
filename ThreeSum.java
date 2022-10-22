ee8a688dc0de288a6c8691cca08045dc943a70bd
A-Rubenstein <arielr@knights.ucf.edu>
2022-10-22 11:35:39 -0400

    final

--- ThreeSum.java
+++ ThreeSum.java
@@ -5,9 +5,9 @@ public class ThreeSum {
 
     public static void main(String[] args) {
         
-        int[] random = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] random = {7,2,1,1,3,4,5,4};
 
-        int target = 24; //enter a target
+        int target = 18; //enter a target
 
         System.out.println("test");
         HashMap hmap1 = new HashMap();
@@ -16,7 +16,7 @@ public class ThreeSum {
         for(int i =0; i<random.length;i++){
             for(int j=0; j<random.length; j++){
                 cnt++;
-                max = (random[i]>max) ? random[i] : max;
+                max = (random[i]>max) ? random[i] : max; //gets rid of duplicate sum issue in hmap
                 int curSum = random[i] + random[j];
                 int compliment = target - curSum;
                 if(hmap1.containsKey(compliment)&&compliment<=max){
